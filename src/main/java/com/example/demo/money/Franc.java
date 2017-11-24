8da263b5c9718f4366f42ba0deae5e085016a44d
bufferings <bufferings@gmail.com>
2017-11-24 23:29:29 +0900

    6. FrancもMoneyを継承するように変更

--- src/main/java/com/example/demo/money/Franc.java
+++ src/main/java/com/example/demo/money/Franc.java
@@ -1,8 +1,6 @@
 package com.example.demo.money;
 
-class Franc {
-
-  private int amount;
+class Franc extends Money {
 
   Franc(int amount) {
     this.amount = amount;
@@ -14,8 +12,8 @@ class Franc {
 
   @Override
   public boolean equals(Object obj) {
-    Franc dollar = (Franc) obj;
-    return amount == dollar.amount;
+    Franc franc = (Franc) obj;
+    return amount == franc.amount;
   }
 
 }
