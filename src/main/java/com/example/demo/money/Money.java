0c6619fdc871d8ca377be257d26d0fb19c497870
bufferings <bufferings@gmail.com>
2017-11-25 00:52:08 +0900

    11. DollarとFrancの生成部分をなくした

--- src/main/java/com/example/demo/money/Money.java
+++ src/main/java/com/example/demo/money/Money.java
@@ -3,11 +3,11 @@ package com.example.demo.money;
 class Money {
 
   static Money dollar(int amount) {
-    return new Dollar(amount, "USD");
+    return new Money(amount, "USD");
   }
 
-  static Franc franc(int amount) {
-    return new Franc(amount, "CHF");
+  static Money franc(int amount) {
+    return new Money(amount, "CHF");
   }
 
   int amount;
