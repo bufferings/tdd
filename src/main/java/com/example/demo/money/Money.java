43feb23887282651ee0dac50bd1b157d3f4ed5e2
bufferings <bufferings@gmail.com>
2017-11-25 00:25:05 +0900

    9. Dollarの生成時にUSDを渡すようにした

--- src/main/java/com/example/demo/money/Money.java
+++ src/main/java/com/example/demo/money/Money.java
@@ -3,7 +3,7 @@ package com.example.demo.money;
 abstract class Money {
 
   static Money dollar(int amount) {
-    return new Dollar(amount, null);
+    return new Dollar(amount, "USD");
   }
 
   static Franc franc(int amount) {
