2044fecc264dc8dd1b754f4ea87aced940c493f5
bufferings <bufferings@gmail.com>
2017-11-24 23:23:36 +0900

    6. equalsメソッドをDollarからMoneyに移動した

--- src/main/java/com/example/demo/money/Dollar.java
+++ src/main/java/com/example/demo/money/Dollar.java
@@ -10,10 +10,4 @@ class Dollar extends Money {
     return new Dollar(amount * multiplier);
   }
 
-  @Override
-  public boolean equals(Object obj) {
-    Dollar dollar = (Dollar) obj;
-    return amount == dollar.amount;
-  }
-
 }
--- src/main/java/com/example/demo/money/Money.java
+++ src/main/java/com/example/demo/money/Money.java
@@ -4,4 +4,10 @@ class Money {
 
   int amount;
 
+  @Override
+  public boolean equals(Object obj) {
+    Dollar dollar = (Dollar) obj;
+    return amount == dollar.amount;
+  }
+
 }
