7e7d4e1a84740d83391485e3ef8a90603de2163c
bufferings <bufferings@gmail.com>
2017-11-24 20:16:04 +0900

    :1. テストを書いた

--- src/test/java/com/example/demo/money/MoneyTest.java
+++ src/test/java/com/example/demo/money/MoneyTest.java
@@ -1,6 +1,20 @@
 package com.example.demo.money;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class MoneyTest {
   // TODO: $5 + 10CHF(スイスフラン) = $10(レートが2:1の場合)
-  // TODO: $5 * 2 = $10
+  // -> TODO: $5 * 2 = $10
+  // TODO: amountをprivateにする
+  // TODO: Dollarの副作用どうする？
+  // TODO: Moneyの丸め処理どうする？
+
+  @Test
+  public void testMultiplication() {
+    Dollar five = new Dollar(5);
+    five.times(2);
+    assertEquals(10, five.amount);
+  }
 }
