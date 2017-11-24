6d86c73f2744b4d88bc601b3b59a0255ed3f1ccb
bufferings <bufferings@gmail.com>
2017-11-24 23:57:01 +0900

    8. [Compile Error] MoneyからFrancを生成できることを期待するテストに変更

--- src/test/java/com/example/demo/money/MoneyTest.java
+++ src/test/java/com/example/demo/money/MoneyTest.java
@@ -41,7 +41,7 @@ public class MoneyTest {
 
   @Test
   public void testFrancMultiplication() {
-    Franc five = new Franc(5);
+    Franc five = Money.franc(5);
     assertEquals(new Franc(10), five.times(2));
     assertEquals(new Franc(15), five.times(3));
   }
