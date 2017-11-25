b018c9a1260ab5b0886e3e2d5b1fd548df147979
bufferings <bufferings@gmail.com>
2017-11-25 12:05:31 +0900

    12. [Compile Error] Moneyのplusメソッドの戻り値の型をExpressionに変更

--- src/main/java/com/example/demo/money/Money.java
+++ src/main/java/com/example/demo/money/Money.java
@@ -38,7 +38,7 @@ class Money {
     return amount + " " + currency;
   }
 
-  public Money plus(Money addend) {
+  public Expression plus(Money addend) {
     return new Money(amount + addend.amount, currency);
   }
 }
