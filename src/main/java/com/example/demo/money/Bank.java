ef4396af97777de40f7273d8f2cfe1887f70155b
bufferings <bufferings@gmail.com>
2017-11-25 13:09:46 +0900

    14. reduceメソッドにBankインスタンスを渡すように変更

--- src/main/java/com/example/demo/money/Bank.java
+++ src/main/java/com/example/demo/money/Bank.java
@@ -3,7 +3,7 @@ package com.example.demo.money;
 class Bank {
 
   Money reduce(Expression source, String to) {
-    return source.reduce(to);
+    return source.reduce(this, to);
   }
 
   void addRate(String from, String to, int rate) {
--- src/main/java/com/example/demo/money/Expression.java
+++ src/main/java/com/example/demo/money/Expression.java
@@ -2,6 +2,6 @@ package com.example.demo.money;
 
 interface Expression {
 
-  Money reduce(String to);
+  Money reduce(Bank bank, String to);
 
 }
