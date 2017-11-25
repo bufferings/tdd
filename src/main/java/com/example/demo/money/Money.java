2a36f9cb059577c9394ab3621f24b2c5671e8fcd
bufferings <bufferings@gmail.com>
2017-11-25 12:49:35 +0900

    13. Money用のreduceの実装をMoneyに持つように変更

--- src/main/java/com/example/demo/money/Money.java
+++ src/main/java/com/example/demo/money/Money.java
@@ -38,7 +38,11 @@ class Money implements Expression {
     return amount + " " + currency;
   }
 
-  public Expression plus(Money addend) {
+  Expression plus(Money addend) {
     return new Sum(this, addend);
+  }
+
+  Money reduce(String to) {
+    return this;
   }
 }
