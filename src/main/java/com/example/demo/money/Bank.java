54645f6975b020752f44bfd022c307057b6f1f93
bufferings <bufferings@gmail.com>
2017-11-25 13:29:44 +0900

    14. [Red] Bankでratesを持つように変更したらどこかでRedになった

--- src/main/java/com/example/demo/money/Bank.java
+++ src/main/java/com/example/demo/money/Bank.java
@@ -1,15 +1,21 @@
 package com.example.demo.money;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Bank {
+
+  private Map<Pair, Integer> rates = new HashMap<>();
 
   Money reduce(Expression source, String to) {
     return source.reduce(this, to);
   }
 
   void addRate(String from, String to, int rate) {
+    rates.put(new Pair(from, to), rate);
   }
 
-  int rate(String currency, String to) {
-    return (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
+  int rate(String from, String to) {
+    return rates.get(new Pair(from, to));
   }
 }
