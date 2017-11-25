caf24c2ba2891824536e41ec1823e34b0a79b72c
bufferings <bufferings@gmail.com>
2017-11-25 13:04:28 +0900

    14. [Compile Error] Moneyクラスで異なる通貨へのreduceを行うテストを追加

--- src/test/java/com/example/demo/money/MoneyTest.java
+++ src/test/java/com/example/demo/money/MoneyTest.java
@@ -11,7 +11,7 @@ public class MoneyTest {
   // TODO: $5 + $5 = $10
   // TODO: $5 + $5がMoneyを返す
   // DONE: Bank.reduce(Money)
-  // TODO: Moneyを変換して換算を行う
+  // -> TODO: Moneyを変換して換算を行う
   // TODO: Reduce(Bank, String)
 
   // TODO: Moneyの丸め処理どうする？
@@ -69,6 +69,14 @@ public class MoneyTest {
   public void testReduceMoney() {
     Bank bank = new Bank();
     Money result = bank.reduce(Money.dollar(1), "USD");
+    assertEquals(Money.dollar(1), result);
+  }
+
+  @Test
+  public void testReduceMoneyDifferentCurrency() {
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Money result = bank.reduce(Money.franc(2), "USD");
     assertEquals(Money.dollar(1), result);
   }
 }
